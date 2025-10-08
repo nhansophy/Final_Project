b7d061fb6d75c45afa9b5b9c6fd72c8beb237c82
unknown <nhansophy066@gmail.com>
2025-10-08 11:45:56 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,36 @@
 
 
 
+let cart = []
+let cartItem=[]
+// Function Display
+
+const Displayproducts = (products = cart) => {
+  let show = ``
+  products.forEach(pro => {
+    show += ` <div class="col-12 col-sm-6 col-md-3">
+      <div class="card h-100">
+        <img src="${pro.image}"
+          class="card-img-top img-fixed" alt="Cappuccino">
+        <div class="card-body">
+          <h5 class="card-title">${pro.name}</h5>
+          <p class="card-text">${pro.description}</p>
+          <p class="fw-bold text-success">${pro.price}$</p>
+          <button  type="button" onclick="AddtoCart(${pro.id})" class="btn btn-warning w-100">Add to cart</button>
+        </div>
+      </div>
+    </div>`
+
+  })
+  document.getElementById("show-product").innerHTML = show
+  Updatetocart()
+ 
+}
+
+fetch("https://tholsreymey.github.io/skincare/skincare.json")
+  .then(res => res.json())
+  .then(item => {
+    cart = item
+    Displayproducts()
+  })
+  .catch(err => alert(err))
